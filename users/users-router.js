cde7e1a66df2031b807938531fda85612fc0f914
lydiathornton <lydia.d.thornton@gmail.com>
2019-04-08 15:44:54 -0700

    MVP Complete

--- users/users-router.js
+++ users/users-router.js
@@ -56,6 +56,7 @@ router.get("/users", restricted, (req, res) => {
     });
 });
 
+//Restricted middleware
 function restricted(req, res, next) {
   const { username, password } = req.headers;
 
@@ -66,7 +67,7 @@ function restricted(req, res, next) {
         if (user && bcrypt.compareSync(password, user.password)) {
           next();
         } else {
-          res.status(401).json({ message: "Invalid Credentials" });
+          res.status(401).json({ message: "You shall not pass!" });
         }
       })
       .catch(error => {
