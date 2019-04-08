6d2a4ae95715e74c90573ed2ad088ab42062ea56
lydiathornton <lydia.d.thornton@gmail.com>
2019-04-08 15:02:29 -0700

    posts are not working

--- users/users-model.js
+++ users/users-model.js
@@ -1,9 +1,27 @@
 const db = require("../database/dbConfig.js");
 
 module.exports = {
-  find
+  find,
+  findBy,
+  findById,
+  add
 };
 
 function find() {
   return db("users").select("id", "username", "password");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+///not working?? use async & await
+async function add(user) {
+  const [id] = await db("users").insert(user);
+  return findById(id);
 }
